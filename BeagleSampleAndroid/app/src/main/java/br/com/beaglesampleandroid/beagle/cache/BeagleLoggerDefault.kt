b80faf085819b078c4f3381a0c3a6fc481dc639f
Luis Eduardo <luis.oliveira@zup.com.br>
2020-12-15 22:00:44 -0300

    created beagle default logger, httpclient and cache

--- BeagleSampleAndroid/app/src/main/java/br/com/beaglesampleandroid/beagle/cache/BeagleLoggerDefault.kt
+++ BeagleSampleAndroid/app/src/main/java/br/com/beaglesampleandroid/beagle/cache/BeagleLoggerDefault.kt
@@ -16,5 +16,29 @@
 
 package br.com.beaglesampleandroid.beagle.cache
 
-class BeagleLoggerDefault {
+import android.util.Log
+import br.com.zup.beagle.android.annotation.BeagleComponent
+import br.com.zup.beagle.android.logger.BeagleLogger
+
+private const val BEAGLE_TAG = "BeagleSDK"
+
+@BeagleComponent
+class BeagleLoggerDefault : BeagleLogger {
+
+    override fun warning(message: String) {
+        Log.w(BEAGLE_TAG, message)
+    }
+
+    override fun error(message: String) {
+        Log.e(BEAGLE_TAG, message)
+    }
+
+    override fun error(message: String, throwable: Throwable) {
+        Log.e(BEAGLE_TAG, message, throwable)
+    }
+
+    override fun info(message: String) {
+        Log.i(BEAGLE_TAG, message)
+    }
+
 }
