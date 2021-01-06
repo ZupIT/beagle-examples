c6c4d575785829c634bc94343bb28c42ba857133
Lucas Fonseca <lucas.fonseca@zup.com.br>
2021-01-06 11:51:23 -0300

    Some alterations

--- BeagleSampleIOS/Beagle Sample/AppDelegate.swift
+++ BeagleSampleIOS/Beagle Sample/AppDelegate.swift
@@ -24,8 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let screen = Beagle.screen(.remote(.init(url: "http://192.168.100.80:8080/login")))
-        //let screen = ItiHomeViewController()
+//        let screen = Beagle.screen(.remote(.init(url: "http://192.168.100.80:8080/login")))
+        let screen = ItiHomeViewController()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = screen
         window?.makeKeyAndVisible()
