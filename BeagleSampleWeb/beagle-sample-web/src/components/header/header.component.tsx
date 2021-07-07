f21dc1519207300b25905f06f20411067b17066e
Hector <hector.custodio@zup.com.br>
2021-07-07 16:29:03 -0300

    Tweaks web example

--- BeagleSampleWeb/beagle-sample-web/src/components/header/header.component.tsx
+++ BeagleSampleWeb/beagle-sample-web/src/components/header/header.component.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { withRouter, useLocation } from 'react-router-dom'
 import './header.styles.scss'
 
 
-const HeaderComponent = () => {
-
+const HeaderComponent = ({history}: any) => {
+  const location = useLocation()
 
   return (
     <div className="header">
       <img className="header-icon" src="/assets/logo-iti.png" alt="" />
+      {location.pathname != '/' &&
+      <span className="header-logout" onClick={()=>history.push('/')}>Sair</span>
+      }
     </div>
   )
+
 }
 
 
-export default HeaderComponent
+export default withRouter(HeaderComponent)
