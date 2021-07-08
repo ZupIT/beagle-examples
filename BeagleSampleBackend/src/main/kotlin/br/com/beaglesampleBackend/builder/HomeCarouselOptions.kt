6d30db796ed0ef2b91d13fdf9e941d66e0cf2756
Hector <hector.custodio@zup.com.br>
2021-07-08 14:51:34 -0300

    Changed listview example to use multiple templates

--- BeagleSampleBackend/src/main/kotlin/br/com/beaglesampleBackend/builder/HomeCarouselOptions.kt
+++ BeagleSampleBackend/src/main/kotlin/br/com/beaglesampleBackend/builder/HomeCarouselOptions.kt
@@ -9,18 +9,9 @@ import br.com.zup.beagle.widget.action.SendRequest
 import br.com.zup.beagle.widget.action.SetContext
 import br.com.zup.beagle.widget.context.ContextData
 import br.com.zup.beagle.widget.context.expressionOf
-import br.com.zup.beagle.widget.core.AlignSelf
-import br.com.zup.beagle.widget.core.EdgeValue
-import br.com.zup.beagle.widget.core.JustifyContent
-import br.com.zup.beagle.widget.core.ListDirection
-import br.com.zup.beagle.widget.core.Size
-import br.com.zup.beagle.widget.core.UnitType
-import br.com.zup.beagle.widget.core.UnitValue
+import br.com.zup.beagle.widget.core.*
 import br.com.zup.beagle.widget.layout.Container
-import br.com.zup.beagle.widget.ui.Image
-import br.com.zup.beagle.widget.ui.ImagePath
-import br.com.zup.beagle.widget.ui.ListView
-import br.com.zup.beagle.widget.ui.Text
+import br.com.zup.beagle.widget.ui.*
 
 object HomeCarouselOptions {
     fun build() = Container(
@@ -52,21 +43,67 @@ object HomeCarouselOptions {
                             ),
                             dataSource = expressionOf("@{initialContext}"),
                             direction = ListDirection.HORIZONTAL,
-                            template = template()
+                            templates = templatesList()
                     ).setStyle {
-                            size = Size(width = UnitValue(100.0, UnitType.PERCENT))
+                        size = Size(width = UnitValue(100.0, UnitType.PERCENT))
                     }
             )
     )
 
-    private fun template() = Container(
+    private fun templatesList() = listOf<Template>(
+            Template(
+                    expressionOf("@{eq(item.title, 'Pix')}"),
+                    newItemTemplate()
+            ),
+            Template(
+                    true,
+                    simpleItemTemplate()
+            )
+    )
+
+    private fun newItemTemplate() = Container(listOf(
+            Container(
+                    listOf(
+                            Text(
+                                    text = "Novo",
+                                    styleId = "h1",
+                                    textColor = "#ffffff"
+                            ), Image(
+                            ImagePath.Remote(remoteUrl = "@{item.icon}")
+                    ).setStyle {
+                        size = Size(width = UnitValue(40.0, UnitType.REAL), height = UnitValue(40.0, UnitType.REAL))
+                    }
+                    )
+
+            ).setFlex {
+                flexDirection = FlexDirection.ROW
+                justifyContent = JustifyContent.SPACE_BETWEEN
+            },
+
+            Text(
+                    text = "@{item.title}",
+                    styleId = "h1",
+                    textColor = "#ffffff"
+            )
+    )
+    ).setStyle {
+        cornerRadius = CornerRadius(radius = 8.0)
+        backgroundColor = "#ff5592"
+        size = Size(width = UnitValue(140.0, UnitType.REAL), height = UnitValue(140.0, UnitType.REAL))
+        margin = EdgeValue.all(10)
+        padding = EdgeValue.all(10)
+    }.setFlex {
+        justifyContent = JustifyContent.SPACE_BETWEEN
+    }
+
+    private fun simpleItemTemplate() = Container(
             listOf(
                     Image(
                             ImagePath.Remote(remoteUrl = "@{item.icon}")
                     ).setStyle {
-                            size = Size(width = UnitValue(40.0, UnitType.REAL), height = UnitValue(40.0, UnitType.REAL))
+                        size = Size(width = UnitValue(40.0, UnitType.REAL), height = UnitValue(40.0, UnitType.REAL))
                     }.setFlex {
-                            alignSelf = AlignSelf.FLEX_END
+                        alignSelf = AlignSelf.FLEX_END
                     },
                     Text(
                             text = "@{item.title}",
@@ -75,12 +112,12 @@ object HomeCarouselOptions {
                     )
             )
     ).setStyle {
-            cornerRadius = CornerRadius(radius = 8.0)
-            backgroundColor = "#fb5f31"
-            size = Size(width = UnitValue(140.0, UnitType.REAL), height = UnitValue(140.0, UnitType.REAL))
-            margin = EdgeValue.all(10)
-            padding = EdgeValue.all(10)
+        cornerRadius = CornerRadius(radius = 8.0)
+        backgroundColor = "#fb5f31"
+        size = Size(width = UnitValue(140.0, UnitType.REAL), height = UnitValue(140.0, UnitType.REAL))
+        margin = EdgeValue.all(10)
+        padding = EdgeValue.all(10)
     }.setFlex {
-            justifyContent = JustifyContent.SPACE_BETWEEN
+        justifyContent = JustifyContent.SPACE_BETWEEN
     }
 }
