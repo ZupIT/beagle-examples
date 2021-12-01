ecb18be38df4057e027927aa27205a2454d4a963
Hector Custódio <hector.custodio@zup.com.br>
2021-12-01 16:04:47 -0300

    Added Web Hello Beagle Sample
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>

--- BeagleSampleBackend/src/main/kotlin/br/com/beaglesampleBackend/controller/ScreenController.kt
+++ BeagleSampleBackend/src/main/kotlin/br/com/beaglesampleBackend/controller/ScreenController.kt
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin(origins = ["http://localhost:8080"], maxAge = 3600)
+@CrossOrigin(origins = ["*"], maxAge = 3600)
 @RestController
 class ScreenController(
     private val firstScreenBeagleService: ScreenService,
