1c50ff1491448c39795abd86bf91ad51d7a00361
Hector Custódio <hector.custodio@zup.com.br>
2021-12-01 15:23:32 -0300

    Initial refactoring of samples
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>

--- LoginBeagle/Backend/src/main/kotlin/br/com/beaglesampleBackend/service/LoginScreenService.kt
+++
@@ -1,9 +0,0 @@
-package br.com.beaglesampleBackend.service
-
-import br.com.beaglesampleBackend.builder.LoginScreenBuilder
-import org.springframework.stereotype.Service
-
-@Service
-class LoginScreenService {
-    fun createLoginScreen() = LoginScreenBuilder()
-}
